78818f607c480ba9b54c618b5a066e70378ddac1
silvererudite <shamima.hossain19@gmail.com>
2024-06-22 23:44:48 +0600

    Add examples for ways of type casting checking during runtime

--- src/polymorphism/TypeCastingTester.java
+++ src/polymorphism/TypeCastingTester.java
@@ -7,20 +7,32 @@ public class TypeCastingTester {
 //
 //        Movie conjuring = new Comedy("Conjuring");
 //        conjuring.watchMovie();
-        Comedy conjuring = (Comedy) Movie.makeMovie("Conjuring", "C");
-        conjuring.watchMovie();
 
-        Object conjuringTwo = Movie.makeMovie("Conjuring 2", "C");
+//        Comedy conjuring = (Comedy) Movie.makeMovie("Conjuring", "C");
+//        conjuring.watchMovie();
+//
+//        Object conjuringTwo = Movie.makeMovie("Conjuring 2", "C");
         //((Movie) conjuringTwo).watchMovie();
 
 //        Movie comedyMovie  = (Comedy) conjuringTwo;
 //        comedyMovie.watchComedy();
 
-        Comedy comedyMovie  = (Comedy) conjuringTwo;
-        comedyMovie.watchComedy();
+//        Comedy comedyMovie  = (Comedy) conjuringTwo;
+//        comedyMovie.watchComedy();
+//
+//        var airplane = Movie.makeMovie("A","Ab");
+//        airplane.watchMovie();
 
-        var airplane = Movie.makeMovie("A","Ab");
-        airplane.watchMovie();
+        Object unknownMovie = Movie.makeMovie("C", "S");
+        if (unknownMovie.getClass().getSimpleName().equals("Comedy")){
+            Comedy c =  (Comedy) unknownMovie;
+            c.watchComedy();
+        } else if (unknownMovie instanceof Adventure) {
+            Adventure a =  (Adventure) unknownMovie;
+            a.watchAdventure();
+        }else if(unknownMovie instanceof SciFi sci){
+            sci.watchSciFi();
+        }
 
     }
 }
