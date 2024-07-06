499cc25e0847d284e576cda7727de610bb2c581d
silvererudite <shamima.hossain19@gmail.com>
2024-07-06 21:50:57 +0600

    Add example for basic concurrency

---
+++ src/concurrency/AnotherThread.java
@@ -0,0 +1,8 @@
+package concurrency;
+
+public class AnotherThread extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Hello from another thread");
+    }
+}
---
+++ src/concurrency/Main.java
@@ -0,0 +1,17 @@
+package concurrency;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main thread");
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+
+        new Thread(){
+            public void run(){
+                System.out.println("from anonymous thread class");
+            }
+        }.start();
+        System.out.println("Hello again from the main thread");
+    }
+}
